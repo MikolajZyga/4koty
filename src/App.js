06f61d2b438bbb8775d744c190cae53250430255
Mikołaj Zyga <mikolaj.zyga@gmail.com>
2024-11-05 13:05:56 +0100

    Fix Vercel Analytics integration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import { Analytics } from '@vercel/analytics';
+import React, { useEffect } from 'react';
+import { inject } from '@vercel/analytics';
 import KalkulatorStandardow from './components/KalkulatorStandardow';
 
 function App() {
+  useEffect(() => {
+    inject();
+  }, []);
+
   return (
     <div className="App">
       <KalkulatorStandardow />
-      <Analytics />
     </div>
   );
 }
